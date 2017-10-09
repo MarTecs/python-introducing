acbe3774e6f871207fcd9016d956f58e160ba62f
sivanWu0222 <sivan0222@vip.qq.com>
2017-10-09 11:41:30 +0800

    modyfy files

---
+++ IntroducingPython/Unit/1/Test1.py
@@ -0,0 +1,11 @@
+
+cichles = [
+    "At the end of the day",
+    "Having said that",
+    "The fact of the matter is",
+    "Be that as it may",
+    "The bottom line is",
+    "If you will",
+]
+
+print(cichles[5])
