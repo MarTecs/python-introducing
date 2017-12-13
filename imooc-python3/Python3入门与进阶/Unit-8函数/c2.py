eb5e2a5b85babbc73c0e6b32490c49694ec6bf73
sivanWu0222 <sivan0222@vip.qq.com>
2017-12-13 19:02:05 +0800

    add

--- imooc-python3/Python3入门与进阶/Unit-8函数/c2.py
+++ imooc-python3/Python3入门与进阶/Unit-8函数/c2.py
@@ -27,4 +27,16 @@ def print(x):
 '''
 
 
-## python中一个函数返回多个值
+## python中一个函数返回多个值，将会以元祖的形式进行返回，但是不建议这样使用
+def para(para1, para2):
+    return para1, para2;
+
+print(type(para(1,2))); #将会是一个元祖
+
+result = para(1,2);
+print(result[0], result[1]);    ##不建议这样获得结果值
+dama1 , dama2 = para(1,2);      ##建议这样获得结果值，变量名更加有意义(相对使用序号来说)
+print(dama1, dama2)
+
+## 多
+
