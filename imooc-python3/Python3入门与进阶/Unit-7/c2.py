f3916b129c3ea6d71ed9620b95897593a872bc86
sivanWu0222 <sivan0222@vip.qq.com>
2017-12-13 13:46:55 +0800

    modify python file

--- imooc-python3/Python3入门与进阶/Unit-7/c2.py
+++ imooc-python3/Python3入门与进阶/Unit-7/c2.py
@@ -8,3 +8,18 @@ for i in fruits:
     for y in i:
         # print(y)  采用这种方式将会默认有一个换行！也就是等同于print(y,"\n");
         print(y,end="\t");
+
+for i in fruits[1]:
+    if i == 2:
+        break;
+    print(end="\n");
+    print(i);
+
+
+## 需要额外注意，当我们使用break终止循环的时候，else分支语句并不会执行！！！
+for i in fruits[1]:
+    if i == 2:
+        break;
+    print(i);
+else:
+    print("EOF");
