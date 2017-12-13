897f955d27483f1316762e8bf4eff20d0d4e1491
sivanWu0222 <sivan0222@vip.qq.com>
2017-12-13 16:14:19 +0800

    modify python files

---
+++ imooc-python3/Python3入门与进阶/Unit-7/c7.py
@@ -0,0 +1,6 @@
+#-*-coding:utf-8-*-
+# Author: sivan
+# computer: Deskttop
+# description:
+
+a = 1;
---
+++ imooc-python3/Python3入门与进阶/Unit-7/c8.py
@@ -0,0 +1,8 @@
+## import只可以导入模块
+import c7;
+
+print(c7.a);
+
+## 可以将导入的模块取一个别名
+import c7 as c;
+print(c.a);
