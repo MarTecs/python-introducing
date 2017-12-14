7520a73b6fb1eca4996b79302d93546d85db3e78
sivanWu0222 <sivan0222@vip.qq.com>
2017-12-14 12:18:55 +0800

    modify python files

--- imooc-python3/Python3入门与进阶/Unit-9面向对象/Student.py
+++ imooc-python3/Python3入门与进阶/Unit-9面向对象/Student.py
@@ -6,9 +6,21 @@ class Student():
     name = '';
     age = 0;
 
+    '''
+    如果按照下面这样写类中的方法将会报错，因为类中的方法必须传入一个参数也就是self
     def print_file():
         print("name：" + name);
         print("age：" + str(age));
-
+    '''
+    '''
+    如果按照下面这样写，将会报错name is undefined错误
+    def print_file(self):
+        print("name：" + name);
+        print("age：" + age);
+student = Student();
+student.print_file();
+    '''
+    def print_file(self):
+        print("name：" + self.name)
 student = Student();
 student.print_file();
