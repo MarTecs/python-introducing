0cd909f3e20441c0a8eff28b4428d0175d441092
sivanWu0222 <sivan0222@vip.qq.com>
2017-12-14 13:38:22 +0800

    modify python files

--- imooc-python3/Python3入门与进阶/Unit-9面向对象/Student.py
+++ imooc-python3/Python3入门与进阶/Unit-9面向对象/Student.py
@@ -27,13 +27,21 @@ student.print_file();
     ## 定义一个构造函数,Python要求构造函数只能返回None，如果返回其他就会报错
     def __init__(self, name, age):
         print("构造函数被调用了!");
+        #name = name; ##Python会自动进行区分，但是只是自己进行赋值
         self.name = name;
         self.age = age;
-
+'''
 student = Student();
 student.print_file();
+
+'''
+
 '''
 测试Python的构造方法
 student2 = Student();
 print(student2.__init__()); ##这样也可以调用构造函数，只是返回一个None
 '''
+
+student2 = Student("吴晓文",18);
+print(student2.age);
+print(student2.name);
