8b741e5e03c87d81330a3c3f42fb7d69a7b66e69
sivanWu0222 <sivan0222@vip.qq.com>
2017-12-14 12:13:17 +0800

    add python files

--- imooc-python3/Python3入门与进阶/Unit-9面向对象/Student.py
+++ imooc-python3/Python3入门与进阶/Unit-9面向对象/Student.py
@@ -3,4 +3,12 @@
 # computer: Deskttop
 # description: 面向对象
 class Student():
+    name = '';
+    age = 0;
 
+    def print_file():
+        print("name：" + name);
+        print("age：" + str(age));
+
+student = Student();
+student.print_file();
