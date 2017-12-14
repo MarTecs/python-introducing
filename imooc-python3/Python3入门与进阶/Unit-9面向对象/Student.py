30fda8b8a697adacbe043462b3f994f279471f16
sivanWu0222 <sivan0222@vip.qq.com>
2017-12-14 13:25:46 +0800

    add

--- imooc-python3/Python3入门与进阶/Unit-9面向对象/Student.py
+++ imooc-python3/Python3入门与进阶/Unit-9面向对象/Student.py
@@ -23,5 +23,13 @@ student.print_file();
     def print_file(self):
         print("name：" + self.name);
         print("age：" + str(self.age));
+
+    ## 定义一个构造函数,Python要求构造函数只能返回None，如果返回其他就会报错
+    def __init__(self):
+        print("构造函数被调用了!");
+
 student = Student();
 student.print_file();
+
+student2 = Student();
+print(student2.__init__()); ##这样也可以调用构造函数，只是返回一个None
--- imooc-python3/Python3入门与进阶/Unit-9面向对象/c2.py
+++ imooc-python3/Python3入门与进阶/Unit-9面向对象/c2.py
@@ -1,4 +1,9 @@
 #-*-coding:utf-8-*-
 # Author: sivan
 # computer: Deskttop
-# description: 
+# description: 调用Student模块中的类
+
+from Student import Student
+
+student = Student()
+student.print_file()
