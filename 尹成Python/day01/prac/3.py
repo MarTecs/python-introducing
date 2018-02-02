f1b7bc93c9b7681b686ca39949294945d11f10d6
sivanWu0222 <sivan0222@vip.qq.com>
2018-02-02 22:53:53 +0800

    编写程序显示下面模式

---
+++ 尹成Python/day01/prac/3.py
@@ -0,0 +1,9 @@
+# -*-coding:utf-8-*-
+# Author: sivan
+# computer: notebook
+# description: 编写程序显示下面的模式
+print("FFFFFFF   U     U  NN      NN") # 3 5 2 5
+print("FF        U     U  NNN     NN")
+print("FFFFFFF   U     U  NN N    NN")
+print("FF         U   U   NN  N   NN")
+print("FF          UUU    NN     NNN")
