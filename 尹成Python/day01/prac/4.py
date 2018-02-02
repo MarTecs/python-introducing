ad72fdef5bd11240c2f7cdb09b24c1e1da0090cd
sivanWu0222 <sivan0222@vip.qq.com>
2018-02-02 22:56:18 +0800

    编写程序显示下面的表格

---
+++ 尹成Python/day01/prac/4.py
@@ -0,0 +1,9 @@
+# -*-coding:utf-8-*-
+# Author: sivan
+# computer: notebook
+# description: 编写程序显示下面的表格
+print("a\ta^2\ta^3")
+print("1\t1\t1\t")
+print("2\t4\t8")
+print("3\t9\t27")
+print("4\t16\t64")
