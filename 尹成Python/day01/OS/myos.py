193c8a3e33882de2dd8c9566306d03084e71d42d
sivanWu0222 <sivan0222@vip.qq.com>
2018-02-02 17:23:12 +0800

    add os

---
+++ 尹成Python/day01/OS/myos.py
@@ -0,0 +1,17 @@
+# -*-coding:utf-8-*-
+# Author: sivan
+# computer: notebook
+# description: 执行CMD命令
+
+import os   #导入操作系统
+#os.system("shutdown -s -t 180")    #180秒后关机
+#os.system("shutdown -a")           #取消定时关机
+
+
+
+
+#os.system("mspaint")                #打开画图板
+#os.system("taskkill /f /im mspaint.exe")#结束画图板进程
+
+
+os.system("ipconfig")   ## 此时将会打印乱码
