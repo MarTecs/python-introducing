be166c8b5b080f2def37834e91df02e074add976
sivanWu0222 <sivan0222@vip.qq.com>
2018-02-02 23:15:07 +0800

    圆的面积和周长

---
+++ 尹成Python/day01/prac/8.py
@@ -0,0 +1,9 @@
+# -*-coding:utf-8-*-
+# Author: sivan
+# computer: notebook
+# description: 圆的面积和周长
+import math
+
+
+print("半径为5.5的圆的面积为：", math.pi * 5.5 * 5.5)
+print("半径为5.5的圆的周长为；", math.pi * 2 * 5.5)
