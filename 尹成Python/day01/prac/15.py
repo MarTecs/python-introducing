e37cbe6c505327fedfe8eadff23309a7704df8e0
sivanWu0222 <sivan0222@vip.qq.com>
2018-02-03 09:23:08 +0800

    绘制两个三角形

---
+++ 尹成Python/day01/prac/15.py
@@ -0,0 +1,25 @@
+# -*-coding:utf-8-*-
+# Author: sivan
+# computer: notebook
+# description: 绘制两个三角形
+
+import turtle
+
+turtle.right(60)
+turtle.penup()
+turtle.back(100)
+turtle.down()
+turtle.forward(200)
+turtle.right(120)
+turtle.forward(100)
+turtle.right(120)
+turtle.forward(200)
+turtle.left(120)
+turtle.forward(100)
+turtle.left(120)
+turtle.forward(100)
+
+
+
+
+turtle.done()
