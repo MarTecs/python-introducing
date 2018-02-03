281399046f8c057fb2e8b8ebb4dbfeb8ef4967e8
sivanWu0222 <sivan0222@vip.qq.com>
2018-02-03 08:51:01 +0800

    绘制正方形

---
+++ 尹成Python/day01/prac/12.py
@@ -0,0 +1,32 @@
+# -*-coding:utf-8-*-
+# Author: sivan
+# computer: notebook
+# description: 编写程序在屏幕中心绘制正方形
+
+import turtle
+turtle.penup()
+turtle.goto(-50,0)
+turtle.pendown()
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+
+
+turtle.penup()
+turtle.goto(0,50)
+turtle.pendown()
+turtle.left(180)
+turtle.forward(100)
+turtle.back(50)
+
+
+
+turtle.done()
