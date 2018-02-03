36e757d2e5cfd62b7602fbfaf5990c1454955345
sivanWu0222 <sivan0222@vip.qq.com>
2018-02-03 09:31:28 +0800

    绘制四个圆

---
+++ 尹成Python/day01/prac/16.py
@@ -0,0 +1,29 @@
+# -*-coding:utf-8-*-
+# Author: sivan
+# computer: notebook
+# description: 绘制4个圆
+
+
+import turtle
+turtle.penup()
+turtle.goto(50,50)
+turtle.pendown()
+turtle.circle(50)
+
+turtle.penup()
+turtle.goto(-50, 50)
+turtle.pendown()
+turtle.circle(50)
+
+turtle.penup()
+turtle.goto(-50, -50)
+turtle.pendown()
+turtle.circle(50)
+
+turtle.penup()
+turtle.goto(50, -50)
+turtle.pendown()
+turtle.circle(50)
+
+
+turtle.done()
