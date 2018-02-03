d7310f3302425ca916a93dde0e417b7d157ff946
sivanWu0222 <sivan0222@vip.qq.com>
2018-02-03 09:15:41 +0800

    绘制三角形

---
+++ 尹成Python/day01/prac/14.py
@@ -0,0 +1,16 @@
+# -*-coding:utf-8-*-
+# Author: sivan
+# computer: notebook
+# description: 绘制三角形
+
+
+import turtle
+turtle.right(60)
+turtle.forward(100)
+turtle.right(120)
+turtle.forward(100)
+turtle.right(120)
+turtle.forward(100)
+
+
+turtle.done()
