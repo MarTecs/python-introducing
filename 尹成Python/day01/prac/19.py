24840dce07cacf8b72b967cf09557b4e64064289
sivanWu0222 <sivan0222@vip.qq.com>
2018-02-03 10:45:36 +0800

    绘制多边形

---
+++ 尹成Python/day01/prac/19.py
@@ -0,0 +1,23 @@
+# -*-coding:utf-8-*-
+# Author: sivan
+# computer: notebook
+# description: 绘制多边形
+
+
+import turtle
+
+turtle.penup()
+turtle.goto(40,-69.28)
+turtle.pendown()
+turtle.goto(-40,-69.28)
+turtle.goto(-80,-9.8)
+turtle.goto(-40,69)
+turtle.goto(40,69)
+turtle.goto(80,0)
+turtle.goto(40,-69.28)
+
+
+turtle.done()
+
+
+
