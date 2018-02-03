f2ecc4b4110f279884e220b077a0dc0c19eb7555
sivanWu0222 <sivan0222@vip.qq.com>
2018-02-03 10:11:13 +0800

    绘制直线

---
+++ 尹成Python/day01/prac/17.py
@@ -0,0 +1,15 @@
+# -*-coding:utf-8-*-
+# Author: sivan
+# computer: notebook
+# description: 绘制直线
+
+
+import turtle
+turtle.penup()
+turtle.goto(-39, 48)
+turtle.pendown()
+turtle.color("red")
+turtle.write("(-39,48)")
+turtle.goto(50,-50)
+turtle.write("(50,-50)")
+turtle.done()
