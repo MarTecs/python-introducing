3af875f46e9d24e287d369612a55780124586ae4
sivanWu0222 <sivan0222@vip.qq.com>
2018-02-03 10:26:34 +0800

    绘制五角星

---
+++ 尹成Python/day01/prac/18.py
@@ -0,0 +1,21 @@
+# -*-coding:utf-8-*-
+# Author: sivan
+# computer: notebook
+# description: 绘制五角星
+
+
+import turtle
+turtle.right(72)
+turtle.forward(100)
+turtle.right(144)
+turtle.forward(100)
+turtle.right(144)
+turtle.forward(100)
+turtle.right(144)
+turtle.forward(100)
+turtle.right(144)
+turtle.forward(100)
+
+
+
+turtle.done()
