b5059135c123729cc5099080bfb6fc49952f9560
sivanWu0222 <sivan0222@vip.qq.com>
2018-02-03 10:56:33 +0800

    显示立方体

---
+++ 尹成Python/day01/prac/20.py
@@ -0,0 +1,40 @@
+# -*-coding:utf-8-*-
+# Author: sivan
+# computer: notebook
+# description: 显示立方体
+
+import turtle
+turtle.penup()
+turtle.goto(-50, 20)
+turtle.pendown()
+turtle.goto(50,20)
+turtle.left(45)
+turtle.forward(40)
+turtle.left(135)
+turtle.forward(100)
+turtle.left(45)
+turtle.forward(40)
+turtle.goto(-50,-20)
+turtle.goto(50,-20)
+turtle.goto(50,20)
+turtle.back(40)
+turtle.left(45)
+turtle.forward(40)
+turtle.right(45)
+turtle.forward(40)
+turtle.back(40)
+turtle.right(45)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(40)
+turtle.back(40)
+turtle.left(135)
+turtle.forward(40)
+
+
+
+
+
+
+
+turtle.done()
