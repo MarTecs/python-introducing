b60f77aa705cd67a619dbd55d130f43c032e461c
sivanWu0222 <sivan0222@vip.qq.com>
2018-02-03 09:11:31 +0800

    绘制十字

---
+++ 尹成Python/day01/prac/13.py
@@ -0,0 +1,20 @@
+# -*-coding:utf-8-*-
+# Author: sivan
+# computer: notebook
+# description: 编写程序绘制十字
+import turtle
+
+turtle.penup()
+turtle.goto(-50, 0)
+turtle.pendown()
+turtle.forward(100)
+
+turtle.penup()
+turtle.goto(0, 50)
+turtle.pendown()
+turtle.right(90)
+turtle.forward(100)
+
+
+turtle.done()
+
