8ac2f4759b48dd8f7ff98515398e1ed030db87af
sivanWu0222 <sivan0222@vip.qq.com>
2018-02-04 09:37:21 +0800

    绘制一个矩形

---
+++ 尹成Python/day01plus/第二章习题/25.py
@@ -0,0 +1,33 @@
+# -*-coding:utf-8-*-
+# Author: sivan
+# computer: notebook
+# description: 绘制一个矩形
+
+import turtle
+
+coordinate = input("请输入矩形中心的坐标：")
+coordinate_x = eval(coordinate.split(',')[0])
+coordinate_y = eval(coordinate.split(',')[1])
+
+length = eval(input("请输入矩形的长度："))
+width = eval(input("请输入矩形的宽度："))
+
+turtle.penup()
+turtle.goto(coordinate_x - length / 2, coordinate_y - width / 2)
+turtle.pendown()
+turtle.forward(length)
+turtle.left(90)
+turtle.forward(width)
+turtle.left(90)
+turtle.forward(length)
+turtle.left(90)
+turtle.forward(width)
+
+
+turtle.done()
+
+
+
+
+
+
