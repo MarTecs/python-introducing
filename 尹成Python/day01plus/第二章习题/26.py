ad0739e79a59b1e768e4b42f5cbcfad60f9e1be4
sivanWu0222 <sivan0222@vip.qq.com>
2018-02-04 09:05:20 +0800

    绘制一个圆

---
+++ 尹成Python/day01plus/第二章习题/26.py
@@ -0,0 +1,25 @@
+# -*-coding:utf-8-*-
+# Author: sivan
+# computer: notebook
+# description: 绘制一个圆
+
+import turtle
+
+radius = eval(input("请输入圆的半径："))
+coordinate_x = eval(input("请输入圆心的横坐标："))
+coordinate_y = eval(input("请输入圆心的纵坐标："))
+
+
+turtle.write("(0,0)")
+turtle.penup()
+turtle.goto(coordinate_x, coordinate_y - radius)
+turtle.pendown()
+turtle.circle(radius)
+
+
+turtle.done()
+
+
+
+
+
