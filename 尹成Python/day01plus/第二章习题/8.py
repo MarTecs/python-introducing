201db3b23d5af0bc0ca1bdc728caf7c20aff0c69
sivanWu0222 <sivan0222@vip.qq.com>
2018-02-10 21:56:07 +0800

    add

--- 尹成Python/day01plus/第二章习题/8.py
+++ 尹成Python/day01plus/第二章习题/8.py
@@ -7,4 +7,6 @@ M = eval(input("Enter the amount of water in kilograms："))
 initialTemperatute = eval(input("Enter the initial temperature："))
 finalTemperature = eval(input("Enter the final temperature："))
 print("The final needed is %f" % (M * (finalTemperature - initialTemperatute) * 4184) )
-##
+
+
+######
