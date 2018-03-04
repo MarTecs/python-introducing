b231714bcc39e801f9814956eb6c8ec2d6788ab0
sivanWu0222 <sivan0222@vip.qq.com>
2018-03-04 20:36:01 +0800

    计算能量

--- 尹成Python/day01plus/第二章习题/8.py
+++ 尹成Python/day01plus/第二章习题/8.py
@@ -6,6 +6,6 @@
 M = eval(input("Enter the amount of water in kilograms："))
 initialTemperatute = eval(input("Enter the initial temperature："))
 finalTemperature = eval(input("Enter the final temperature："))
-print("The final needed is %f" % (M * (finalTemperature - initialTemperatute) * 4184) )
-
+Q = M * (finalTemperature - initialTemperatute) * 4184
+print("The energy needed is %.1f" % Q)
 
