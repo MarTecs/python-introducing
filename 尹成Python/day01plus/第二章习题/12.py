450f0983ebd98ef0bc71a91c94fab2f5e0390c5a
sivanWu0222 <sivan0222@vip.qq.com>
2018-03-05 08:44:24 +0800

    打印表格

---
+++ 尹成Python/day01plus/第二章习题/12.py
@@ -0,0 +1,11 @@
+# -*-coding:utf-8-*-
+# Author: sivan
+# computer: notebook
+# description: 打印表格
+
+print("a\tb\ta ** b")
+print("1\t2\t1")
+print("2\t3\t8")
+print("3\t4\t81")
+print("4\t5\t1024")
+print("5\t6\t15625")
