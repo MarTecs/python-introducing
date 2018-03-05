de9066033a8771b4886983976ca9913fc1caf277
sivanWu0222 <sivan0222@vip.qq.com>
2018-03-05 09:15:56 +0800

    几何方面：正六边形的面积

---
+++ 尹成Python/day01plus/第二章习题/15.py
@@ -0,0 +1,10 @@
+# -*-coding:utf-8-*-
+# Author: sivan
+# computer: notebook
+# description: 几何方面：正六变形的面积
+
+
+import math
+side = eval(input("Enter the side："))
+area = math.sqrt(3) * 3  * (side ** 2) / 2
+print("The area of the hexagon is %f" % area)
