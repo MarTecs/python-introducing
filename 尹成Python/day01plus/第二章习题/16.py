a1f75efe6303ade0557a00735874e4766c5f2101
sivanWu0222 <sivan0222@vip.qq.com>
2018-03-05 09:18:01 +0800

    物理方面：加速度

---
+++ 尹成Python/day01plus/第二章习题/16.py
@@ -0,0 +1,9 @@
+# -*-coding:utf-8-*-
+# Author: sivan
+# computer: notebook
+# description: 物理方面：加速度
+
+v0, v1, t = eval(input("Enter v0, v1 and t："))
+
+a = ( v1 - v0 ) / t
+print("The average acceleration is %f" % a)
