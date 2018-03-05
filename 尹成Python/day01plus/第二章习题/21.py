f0728858792ba9bd41c9273680978ba64e266d68
sivanWu0222 <sivan0222@vip.qq.com>
2018-03-05 15:35:08 +0800

    金融应用程序：复利值

--- 尹成Python/day01plus/第二章习题/21.py
+++ 尹成Python/day01plus/第二章习题/21.py
@@ -4,7 +4,13 @@
 # description: 金融应用程序，复利值
 
 
-rate_year = 0.05
+rate_year = 0.00417
 money = eval(input("请输入你要存款的金额："))
 
+for i in range(6):
+    if i != 0:
+        money = money + 100
+    money = money * (1 + rate_year)
+    print(money)
 
+print("After the sixth month, the account value is %f" % money)
