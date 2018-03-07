d6e41cec66c01a602a1e8d4f00d43578ef3b72d7
sivanWu0222 <sivan0222@vip.qq.com>
2018-03-07 11:19:17 +0800

    round函数注意事项

--- 尹成Python/day01plus/20round.py
+++ 尹成Python/day01plus/20round.py
@@ -1,8 +1,8 @@
 # -*-coding:utf-8-*-
 # Author: sivan
 # computer: notebook
-# description: 使用round函数进行四舍五入
+# description: 使用round函数进行取值，并不一定是四舍五入，当round函数中的数与两个整数的接近程度相同，那么将会返回偶数
 
 
-num = 40.5
+num = 41.1
 print(round(num))
